1a7856de838fc0ed62dddc7be697007f31e886b1
unknown <myzakharov@yandex.ru>
2022-07-31 23:06:23 +0500

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let arr = [];
+    
+    for(let i=0; i<str.length; i++) {
+      for(let j=0; j<bracketsConfig.length; j++) {
+        if (str[i] === bracketsConfig[j][0] && str[i] === bracketsConfig[j][1]) {
+          if (arr.includes(str[i])) {
+            symb = arr.pop();
+            if (symb != bracketsConfig[j][0])
+              return false;           
+          }
+          else {
+            arr.push(str[i]);          
+          }
+        }
+  
+        if (str[i] === bracketsConfig[j][0] && str[i] !== bracketsConfig[j][1])
+          arr.push(str[i]);
+        if (str[i] !== bracketsConfig[j][0] && str[i] === bracketsConfig[j][1]) {
+          symb = arr.pop();
+          if (symb != bracketsConfig[j][0])
+            return false;        
+        }
+      }
+    }
+    
+    if(arr.length === 0)
+      return true;
+    else
+      return false;
+  
 }
